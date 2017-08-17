2cd01bcc4163ef135898601c0498e12a371c17f0
Breno Batista Machado <brenomachado@gmail.com>
2017-08-17 14:10:15 -0300

    First commit

---
+++ AppMVCTestSCA/Controllers/Base/CustomController.cs
@@ -0,0 +1,18 @@
+﻿using CEB.SCA.Services.Spec.Services;
+using CEB.SCA.WebControls.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMVCTestSCA.Controllers.Base
+{
+    public class CustomController : SCAController
+    {
+        public override IAutenticacaoService GetAutenticacaoService()
+        {
+            return serviceLocator.GetService<IAutenticacaoService>("ceb.servicos/AutenticacaoService", SCAApplicationContext.ObterParametrosServico(), "userinfo");
+        }
+    }
+}
